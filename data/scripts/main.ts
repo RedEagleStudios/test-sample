2ca51ccd4039fc006ec465d754f2735a0c9b23d4
DjakaTechnology <djakajaya89@gmail.com>
2024-04-30 23:54:26 +0700

    Added comments

--- data/scripts/main.ts
+++ data/scripts/main.ts
@@ -1,9 +1,11 @@
 import { system, world } from "@minecraft/server";
 
+// This will print Hello world every 20 seconds
 system.runInterval(() => {
     world.sendMessage("Hello, world!");
 }, secondToTick(5));
 
+// Convert second to tick, 1 tick in minecraft is 0.05 second. 20 ticks = 1 second
 function secondToTick(second: number) {
     return second * 20
 }
